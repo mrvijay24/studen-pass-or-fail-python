8378d9435a01b5c7bcea55e4a624a32982dc512b
mrvijay24 <110984700+mrvijay24@users.noreply.github.com>
2022-11-24 19:58:47 +0530

    Add files via upload

---
+++ student is pass or fail.py
@@ -0,0 +1,11 @@
+#pass or fail programme
+sno=int(input('s.no'))
+name=input('enter the name:')
+group=input('enter the group:')
+s1=int(input('enter the marks of s1'))
+s2=int(input('enter the marks of s2'))
+s3=int(input('enter the marks of s3'))
+if s1>35 & s2>35 & s3>35:
+    print('pass')
+else:
+        print('fail')
